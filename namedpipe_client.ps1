37974bf74e88b7137e0a0cb11960cf417e45d3a5
phucknguyen <phucknguyen@gmail.com>
2018-12-22 15:18:53 +0700

    Add example scripts for named pipe I/O

---
+++ namedpipe_client.ps1
@@ -0,0 +1,8 @@
+﻿$pipe = new-object System.IO.Pipes.NamedPipeClientStream '.','testpipe','In'
+$pipe.Connect()
+$sr = new-object System.IO.StreamReader $pipe
+while (($data = $sr.ReadLine()) -ne $null) { 
+    "Received: $data" 
+}
+$sr.Dispose()
+$pipe.Dispose()
---
+++ pipe2.ps1
@@ -0,0 +1,11 @@
+﻿$pipe = new-object System.IO.Pipes.NamedPipeClientStream("Solarwinds", 'Solarwinds', [System.IO.Pipes.PipeDirection]::InOut, [System.IO.Pipes.PipeOptions]::None, [System.Security.Principal.TokenImpersonationLevel]::Impersonation)
+$pipe.Connect();  
+$sw = new-object System.IO.StreamWriter($pipe);
+$i = Get-Random -minimum 0 -maximum 100
+$sw.WriteLine($i);
+$sw.Flush();
+
+$sw.WriteLine("exit"); 
+
+$sw.Dispose(); 
+$pipe.Close();
