f8f8f7af76277ee192e243a0c481c1718abc439d
phucknguyen <phucknguyen@gmail.com>
2019-01-01 08:43:56 +0700

    Clean up

--- src/spotlight-wallpaper.ps1
+++ src/spotlight-wallpaper.ps1
@@ -8,20 +8,18 @@ $currentDir=(Get-Location)
 
 $targetDir="${currentDir}/var/spotlight-images"
 If (-not (Test-Path "$targetDir")) {
-  New-Item -Path "$targetDir" -ItemType "directory"
+    New-Item -Path "$targetDir" -ItemType "directory"
 }
 
 # Copy the files from source to target
 Get-ChildItem "$sourceDir" `
-  | Where-Object {$_.Length -ge 10000} `
-  | Select-Object `
-    @{Name="Source"; 
-      Expression={"${sourceDir}/" + $_.Name}}, `
-    @{Name="Target"; 
-      Expression={"${targetDir}/" + $_.Name + ".jpg"}} `
-  | Where-Object {-not (Test-Path $_.Target)} `
-  | ForEach-Object {
-      $source = $_.Source
-      $target = $_.Target
-      Copy-Item "$source" "$target"
-  }
+    | Where-Object {$_.Length -ge 10000} `
+    | Select-Object `
+        @{Name="Source"; Expression={"${sourceDir}/" + $_.Name}}, `
+        @{Name="Target"; Expression={"${targetDir}/" + $_.Name + ".jpg"}} `
+    | Where-Object {-not (Test-Path $_.Target)} `
+    | ForEach-Object {
+        $source = $_.Source
+        $target = $_.Target
+        Copy-Item "$source" "$target"
+    }
